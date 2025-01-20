a9eb60a3293ed96add0d90b94dc13a9a91b447ef
JoeClaborn <joseph.claborn2@gmail.com>
2025-01-20 01:40:01 -0500

    create, start, and finish lab 01

---
+++ CS1160/Labs/SmileyFace.py
@@ -0,0 +1,35 @@
+# Joe Claborn - CS1160 - Lab 01
+
+# Using print statements for Smiley Face pattern
+print("++++++++++++++++")
+print("+              +")
+print("+              +")
+print("+    x    x    +")
+print("+              +")
+print("+ x          x +")
+print("+  x        x  +")
+print("+   x      x   +")
+print("+    xxxxxx    +")
+print("+              +")
+print("+              +")
+print("++++++++++++++++")
+
+# Set a pattern variable to the Smiley Face pattern
+# pattern = [
+#     "++++++++++++++++",
+#     "+              +",
+#     "+              +",
+#     "+    x    x    +",
+#     "+              +",
+#     "+ x          x +",
+#     "+  x        x  +",
+#     "+   x      x   +",
+#     "+    xxxxxx    +",
+#     "+              +",
+#     "+              +",
+#     "++++++++++++++++"
+# ]
+
+# # Use a For-Loop to iterate through the pattern and print to output
+# for line in pattern:
+#     print(line)
