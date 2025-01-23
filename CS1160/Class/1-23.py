d8c8967539501ecae8fab2d3be5bc224aecfd1f8
Joe Claborn <claborn.2@wright.edu>
2025-01-23 15:51:32 -0500

    class file

---
+++ CS1160/Class/1-23.py
@@ -0,0 +1,26 @@
+# This is a sample program for my CS1160 class
+# We are showcasing the print function
+print("Hello World!")
+print("Hello World... again!")
+print("5,000") # I print 5,000 here because I like the number
+print('Please enter your name: ')
+# print('My dog\'s name is Buddy')
+print("The man said "Hello!" and then walked away")
+
+firstName = "Max"
+FirstName = "Jack"
+
+_middleName = "Andrew"
+lastName = "Gilson"
+age0 = 29
+age1 = 29
+age2 = 29
+age3 = 29
+gpa_for_spring_2025 = 4.0
+print(lastName)
+print(_middleName)
+print(firstName)
+print(FirstName)
+print(age0)
+print(lastName, firstName)
+
