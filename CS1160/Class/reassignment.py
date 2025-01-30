e086c88b091dafd53c4de9a7838ebd0eab03e576
Joe Claborn <claborn.2@wright.edu>
2025-01-29 20:10:53 -0500

    renamed & added in-class code

---
+++ CS1160/Class/reassignment.py
@@ -0,0 +1,23 @@
+firstName = "Max"
+print(firstName)
+firstName = "Jack"
+print(firstName)
+firstName = 1234
+print(firstName)
+####### Garbage collection deletes/removes firstName
+lastName = "Gilson"
+print(lastName)
+my_variable_name = -100
+myVariableName = -1
+
+userInput = input("Enter the amount of apples you purchase daily: ")
+apples_purchased_daily = float(userInput)
+applesPurchasedWeekly = apples_purchased_daily * 7  # 7 is a numeric literal
+print("Daily, you purchase", apples_purchased_daily, "apples")
+print("Weekly, you purchase", applesPurchasedWeekly, "apples")
+
+carrotsPurchasedDaily = float(input("Enter the amount of carrots you purchase daily: "))
+carrotsPurchasedWeekly = carrotsPurchasedDaily * 7  # 7 is a numeric literal
+print("Daily, you purchase", carrotsPurchasedDaily, "carrots")
+print("Weekly, you purchase", carrotsPurchasedWeekly, "carrots")
+
