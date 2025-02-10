a5295810cfea10f43d44d5dbb7c845de24357037
Joe Claborn <claborn.2@wright.edu>
2025-02-10 00:46:27 -0500

    finish lab 3

--- CS1160/Labs/average_grade.py
+++ CS1160/Labs/average_grade.py
@@ -1,17 +1,30 @@
 # Joe Claborn - CS1160 - Lab 03
 
+# Print welcome statement
 print("Welcome to the grade estimator.")
 
+# User inputs for each assignment (1-5) and create a tracking variable for the user input amount
+# and add 1 to its total after each user input is given
+userInputTotal = 0
 grade1 = float(input("Please enter your grade for assignment 1: "))
+userInputTotal = userInputTotal + 1
 grade2 = float(input("Please enter your grade for assignment 2: "))
+userInputTotal = userInputTotal + 1
 grade3 = float(input("Please enter your grade for assignment 3: "))
+userInputTotal = userInputTotal + 1
 grade4 = float(input("Please enter your grade for assignment 4: "))
+userInputTotal = userInputTotal + 1
 grade5 = float(input("Please enter your grade for assignment 5: "))
+userInputTotal = userInputTotal + 1
 
-averageGrade = ((grade1 + grade2 + grade3 + grade4 + grade5) / 5)
+# Set averageGrade variable to the user inputs above divided by the amount of inputs
+averageGrade = ((grade1 + grade2 + grade3 + grade4 + grade5) / userInputTotal)
 
-print(f"Your average grade is {averageGrade:.2f}")
+# Print out the average grade to the terminal
+print(f"Your average grade is {averageGrade:.2f}.")
 
+# If-ElseIf-Else statements to check the user's grade that they will get
+# in the class based on the averageGrade variable
 if (averageGrade >= 90.00) :
     print("You will get a A in this class.")
 elif (averageGrade < 90.00 and averageGrade >= 80.00) :
