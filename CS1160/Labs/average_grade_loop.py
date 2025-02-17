c4666a95e80039daacfde265980c024f36883059
JoeClaborn <joseph.claborn2@gmail.com>
2025-02-17 14:56:29 -0500

    lab4 part 1 done

---
+++ CS1160/Labs/average_grade_loop.py
@@ -0,0 +1,40 @@
+# Joe Claborn - CS1160 - Lab 04 - Part 1
+
+# Print welcome statement
+print("Welcome to the grade estimator.")
+
+# Create checkInput variable for checking if the user wants to continue entering grades.
+# Create totalGrade variable to keep check of the running total of the grades entered.
+# Create count variable to keep track of the total number of grades entered to use to
+# find the average grade.
+checkInput = ""
+totalGrade = 0
+count = 0
+
+# While the user's input is not 'n', ask for the grade for the assignment, take the grade
+# given and add it to the total grade, increment count by 1, then find the average grade
+# for each iteration of the while loop. Then, check if the user wants to input more grades,
+# if the input is 'n', exit the loop, if it is 'y', repeat the steps.
+while (checkInput != 'n') :
+    grade = float(input("Please enter your grade for the assignment: "))
+    totalGrade += grade
+    count += 1
+    averageGrade = totalGrade / count
+    checkInput = input("Would you like to enter another grade? (y/n): " )
+    print("\n")
+
+# Print out the average grade to the terminal.
+print(f"Your average grade is {averageGrade:.2f}.")
+
+# If-ElseIf-Else statements to check the user's grade that they will get
+# in the class based on the averageGrade variable.
+if (averageGrade >= 90.00) :
+    print("You will get a A in this class.")
+elif (averageGrade < 90.00 and averageGrade >= 80.00) :
+    print("You will get a B in this class.")
+elif (averageGrade < 80.00 and averageGrade >= 70.00) :
+    print("You will get a C in this class.")
+elif (averageGrade < 70.00 and averageGrade >= 60.00) :
+    print("You will get a D in this class.")
+else :
+    print("You will get a F in this class.")
---
+++ CS1160/Labs/target_weight.py
@@ -0,0 +1,2 @@
+# Joe Claborn - CS1160 - Lab 04 - Part 2
+
