c090814abf1a314890e36ec94bbfd3be5ff19a08
Joe Claborn <joseph.claborn2@gmail.com>
2025-11-17 15:30:47 -0500

    comments

--- valorant_agent_randomizer.py
+++ valorant_agent_randomizer.py
@@ -1,11 +1,13 @@
 import random
 
+# List of Valorant agents
 agents = [
     "Astra", "Breach", "Brimstone", "Chamber", "Clove", "Cypher", "Deadlock", "Fade", "Gekko", "Harbor",
     "Iso", "Jett", "KAY/O", "Killjoy", "Neon", "Omen", "Phoenix", "Raze", "Reyna", "Sage", "Skye", 
     "Sova", "Tejo", "Veto", "Viper", "Vyse", "Waylay", "Yoru"
 ]
 
+# Generate a random number between 0 and 26
 num = random.randint(0, 26)
 print(f'Random number: {num}')
 print(agents[num])
